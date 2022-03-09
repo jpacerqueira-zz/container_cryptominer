87009cf4e934e16ef7cd2b53331bbd15b336c191
jpacerqueira <jpacerqueira@gmail.com>
2022-03-09 09:45:26 +0000

    Changed for generic email - registred in minergate.com/internal

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
   # Adapted to run on a Debian Ubunto Linux
   - https://FuelBigData.com/blog/how-to-do-crypto-miner-with-container
 
-  # Use it as is
+  # Use it with a minergate registration email
 
-  ./start-cryptominer
+  ./start-cryptominer <YOUR_MINERGATE_EMAIL>
 
--- minergate/start-mine-monero-xrm.pool.minergate_v0.1.sh
+++ minergate/start-mine-monero-xrm.pool.minergate_v0.1.sh
@@ -4,7 +4,11 @@
 cd $HOME/cpuminer-multi
 TRDDD=$(date +"%Y-%m-%d")
 #
-./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u joao@fuelbigdata.com  > $HOME/mining-monero-$TRDDD-v0.1.log & 
+if [[ $CRYPTO_EMAIL == "" ]]; then
+   CRYPTO_EMAIL=joao@fuelbigdata.com
+fi
+#
+./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u $CRYPTO_EMAIL  > $HOME/mining-monero-$TRDDD-v0.1.log & 
 #
 sleep 5
 tail -f $HOME/mining-monero-$TRDDD-v0.1.log
